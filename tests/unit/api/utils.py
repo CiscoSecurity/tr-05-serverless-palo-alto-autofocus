eed7fb0a6c78eb2ef3cdb872014491516fa15c9b
TarasMaliuchenko <78420997+TarasMaliuchenko@users.noreply.github.com>
2021-02-24 18:56:28 +0200

    Restructure code (#13)
    * Restructure code
    Add docker deployment
    Add watchdog api
    Add tests for watchdog api
    
    * Minor changes in requirements
    
    * Minor changes in dockerignore
    
    * Minor changes in capabilities and properties of module type
    
    * Change module name

--- tests/unit/api/utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def get_headers(jwt, auth_type='Bearer'):
-    return {'Authorization': f'{auth_type} {jwt}'}
-
-
-def get_from_isoformat(time):
-    return datetime.fromisoformat(time[0:-1])
